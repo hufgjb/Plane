bc64749ffb5649400e0d5e04654470db2e483d15
huf <hufgjb226@gmail.com>
2025-01-03 12:46:05 +0800

    2025.01.03*

--- Assets/plane.cs
+++ Assets/plane.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
-    public float moveSpeed = 1; // 移動速度
+    public GameObject BulletPrefab;
+        public float moveSpeed = 1; // 移動速度
 
     void FixedUpdate()
     {
@@ -17,6 +18,16 @@ public class Plane : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             gameObject.transform.position += new Vector3(-moveSpeed, 0, 0) * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        //增加子彈發射的功能
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = gameObject.transform.position + new Vector3(0, 0.6f, 0); //子彈生成的位置根據戰機的位置，再往上加0.6f
+            Instantiate(BulletPrefab, pos, gameObject.transform.rotation); //依據上述的pos位置，生成子彈
         }
     }
 }
